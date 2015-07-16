c7c8785bcd58422216dcca7ec6bf02cf10bfbdff
Luke Savage <luke.savage92@gmail.com>
2015-07-15 20:27:33 -0500

    Implemented the rendering for SongQueueView

--- client/views/SongQueueView.js
+++ client/views/SongQueueView.js
@@ -1,11 +1,32 @@
 // SongQueueView.js - Defines a backbone view class for the song queue.
 var SongQueueView = Backbone.View.extend({
 
+  tagName: 'table',
+
+
+
   initialize: function() {
+    this.render();
   },
 
   render: function() {
+    this.$el.children().detach();
+
+
+
+    this.$el.html('<th>Song Queue</th>').append(
+      this.collection.map(function(song){
+        return new SongQueueEntryView({model: song}).render();
+      })
+    );
+    console.log("I'm rendering");
     return this.$el;
   }
 
 });
+
+// this.$el.html('<th>Library</th>').append(
+//       this.collection.map(function(song){
+//         return new LibraryEntryView({model: song}).render();
+//       })
+// );
